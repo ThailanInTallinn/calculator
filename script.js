257a55472ccad8c01ae2bdde6517f6002384b8a3
ThailanInTallinn <thailangm@icloud.com>
2024-06-06 21:47:06 +0300

    Creates functions to basic arithmetic operation

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const display = document.querySelector("p");
 const buttonsContainer = document.querySelector("#buttons-container");
+
+function sum(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
