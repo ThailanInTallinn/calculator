76626d4804780b54f83678730003217297b01dcd
ThailanInTallinn <thailangm@icloud.com>
2024-06-10 16:08:26 +0300

    Feat: gets and parses the operands appropriately

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const display = document.querySelector("p");
 const buttonsContainer = document.querySelector("#buttons-container");
-
+let operator = null;
+let firstOperand = "";
+let secondOperand = null;
+let firstOperandNumber = null;
 function sum(a, b) {
 	return a + b;
 }
@@ -16,3 +19,23 @@ function multiply(a, b) {
 function divide(a, b) {
 	return a / b;
 }
+
+buttonsContainer.addEventListener("click", (e) => {
+	if (Number(e.target.textContent)) {
+		display.textContent += `${e.target.textContent}`;
+		inputOperand(e.target.textContent);
+		console.log(firstOperandNumber);
+	} else {
+	}
+});
+
+function inputOperand(operand) {
+	if (operator === null) {
+		firstOperand += operand;
+		firstOperandNumber = Number(firstOperand);
+	} else {
+		secondOperand += operand;
+		secondOperandNumber = Number(secondOperand);
+	}
+};
+
